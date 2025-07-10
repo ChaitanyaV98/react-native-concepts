5315f90d6961c41d48ce33ec249d493ecedcae7e
ChaitanyaV98 <chaitanya@betalectic.com>
2025-07-10 15:48:24 +0530

    added text input

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import { StyleSheet, View } from 'react-native';
 import Basics from './src/components/Basics';
+import TextInputComponent from './src/components/TextInput';
 
 function App(): React.JSX.Element {
   return (
     <View style={styles.container}>
       <Basics />
+      <TextInputComponent />
     </View>
   );
 }
---
+++ src/components/TextInput.tsx
@@ -0,0 +1,39 @@
+import { useState } from 'react';
+import { StyleSheet, View, Text, TextInput } from 'react-native';
+
+function TextInputComponent(): React.JSX.Element {
+  const [value, setValue] = useState('');
+  return (
+    <View style={styles.container}>
+      <Text style={styles.headerText}>Textinput component</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Enter some text here"
+        onChangeText={setValue}
+        value={value}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    marginBottom: 20,
+  },
+  input: {
+    height: 50,
+    borderColor: 'grey',
+    borderWidth: 1,
+    paddingHorizontal: 10,
+    marginBottom: 10,
+  },
+  headerText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 20,
+    marginTop: 20,
+    textDecorationLine: 'underline',
+  },
+});
+
+export default TextInputComponent;
