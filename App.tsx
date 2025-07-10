2e58270dff667685ad545481741f2e5a0f6092e7
ChaitanyaV98 <chaitanya@betalectic.com>
2025-07-10 16:35:04 +0530

    added scroll view and nested scroll view

--- App.tsx
+++ App.tsx
@@ -1,18 +1,28 @@
-import { StyleSheet, View } from 'react-native';
+import { ScrollView, StyleSheet, View } from 'react-native';
 import Basics from './src/components/Basics';
 import TextInputComponent from './src/components/TextInput';
+import ScrollViewComponent from './src/components/ScrollView';
 
 function App(): React.JSX.Element {
   return (
     <View style={styles.container}>
-      <Basics />
-      <TextInputComponent />
+      <ScrollView
+        contentContainerStyle={styles.scrollViewContent}
+        bounces={true}
+        nestedScrollEnabled={true}
+      >
+        <Basics />
+        <Basics />
+        <TextInputComponent />
+        <ScrollViewComponent />
+      </ScrollView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: { flex: 1 },
+  scrollViewContent: { padding: 20 },
 });
 
 export default App;
---
+++ src/components/ScrollView.tsx
@@ -0,0 +1,45 @@
+import { View, StyleSheet, Text, ScrollView } from 'react-native';
+
+function ScrollViewComponent(): React.JSX.Element {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.headerText}>ScrollView</Text>
+      <ScrollView
+        style={styles.scrollView}
+        contentContainerStyle={styles.scrollViewContent}
+        bounces={true}
+        nestedScrollEnabled={true}
+        showsVerticalScrollIndicator={true}
+      >
+        {[...Array(20)].map((_, index) => (
+          <View key={index} style={styles.box}>
+            <Text style={styles.boxText}>{index + 1}</Text>
+          </View>
+        ))}
+      </ScrollView>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    marginBottom: 20,
+  },
+  headerText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 20,
+    marginTop: 20,
+    textDecorationLine: 'underline',
+  },
+  scrollView: { height: 200, backgroundColor: 'f0f0f0' },
+  scrollViewContent: { padding: 20 },
+  box: {
+    height: 40,
+    marginBottom: 10,
+    backgroundColor: '#3498db',
+    justifyContent: 'center',
+  },
+  boxText: { color: '#ffffff', fontWeight: 'bold' },
+});
+export default ScrollViewComponent;
